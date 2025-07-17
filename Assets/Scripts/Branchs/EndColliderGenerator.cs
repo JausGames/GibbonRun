e6ff63847d6cfb1e3052d877fdb74b039c3b9e5b
JausGames <71459206+JausGames@users.noreply.github.com>
2025-07-17 13:47:30 +0200

    Update EndColliderGenerator.cs

--- Assets/Scripts/Branchs/EndColliderGenerator.cs
+++ Assets/Scripts/Branchs/EndColliderGenerator.cs
@@ -1,10 +1,14 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer), typeof(BoxCollider))]
 public class EndColliderGenerator : MonoBehaviour
 {
     public Vector3 boxSize = new Vector3(4f, 4f, 2f); // Width, Height, Depth
     public string connectorName = "Connector";
+    public UnityEvent OnTriggeredEvent { get; } = new UnityEvent();
+
+    private void OnTriggerEnter(Collider other) => onTriggeredEvent.Invoke();
 
     public void GenerateLevelEnd(Branch branch)
     {
