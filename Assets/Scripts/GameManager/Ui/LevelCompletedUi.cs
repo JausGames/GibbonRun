ef4bbae2e80dbb6afb8175ba123091d08ecbe648
JausGames <71459206+JausGames@users.noreply.github.com>
2025-07-17 13:59:40 +0200

    Create LevelCompletedUi.cs

---
+++ Assets/Scripts/GameManager/Ui/LevelCompletedUi.cs
@@ -0,0 +1,11 @@
+using System; 
+using UnityEngine.UnitEvents;
+using UnityEngine.UI;
+
+public class LevelCompletedUi : MonoBehaviour
+{  
+    private UnityEvent NextLevelEvent { get; } = new UnityEvent(); 
+    [SerializedField] private Button nextButton; 
+
+    private void Awake = nextButton.OnClick.AddListener(NextLevelEvent.Invoke); 
+}
